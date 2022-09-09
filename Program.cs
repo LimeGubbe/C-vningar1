e1f6d71f33d5f413ba0504699a4050f407b88f32
Emil L <91726236+LimeGubbe@users.noreply.github.com>
2022-09-09 13:29:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace exercise_05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* 10. Console.WriteLine("I will tell you a story, but need some info first");
+              Console.WriteLine("Tell me the character's name");
+              string name = Console.ReadLine();
+              Console.WriteLine("Now tell me their profession");
+              string prof = Console.ReadLine();
+              Console.WriteLine("Here is the story");
+              Console.WriteLine("Once upon a time there was a proud " + prof + " name of " + name + " they went into goddamn debt and had to sell their computer");
+              Console.WriteLine("The end"); */
+
+
+            /* 11 int days = 200;
+             double hours = 3.5; //använd convert?
+             string coding = "It sure is";
+             Console.WriteLine("Days to summer:");
+             Console.WriteLine(days);
+             Console.WriteLine("Hours to lunch:");
+             Console.WriteLine(hours);
+             Console.WriteLine("Coding is fun:");
+             Console.WriteLine(coding);
+             */
+
+            /* 12 Console.WriteLine("Give a number!");
+              int number = 0;
+              number = int.Parse(Console.ReadLine());
+              Console.WriteLine("You gave " + number); */
+
+            /* 13 Console.WriteLine("Give a decimal number!");
+             double number = 0.0;
+             number = double.Parse(Console.ReadLine());
+             Console.WriteLine("You gave " + number); */
+
+            /* 14 Console.WriteLine("Give me the truth!");
+             bool answer = true;
+             Console.ReadLine();
+             Console.WriteLine(answer); */
+
+
+
+            /* 15  Console.WriteLine("Give a string:");
+               string word = Console.ReadLine();
+               Console.WriteLine("Give an integer");
+               int number = 0;
+               number = int.Parse(Console.ReadLine());
+               Console.WriteLine("Give a double");
+               double dnumber = 0.0;
+               dnumber = double.Parse(Console.ReadLine()); */
+
+            /* 16 Console.WriteLine("How many days?");
+           int number = 0;
+           number = int.Parse(Console.ReadLine());
+           Console.WriteLine(number * 24 * 3600); */
+
+            Console.WriteLine("Give me a number");
+            int number1 = 0;
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give me another number");
+            int number2 = 0;
+            number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(number1 + number2);
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
